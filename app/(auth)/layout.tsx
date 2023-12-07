b41141511f30486582f0d86d47dd3add15d127b3
wungthing <wungthingkashungnao123@gmail.com>
2023-12-07 12:47:05 +0530

    added authentication and authorzation using clerk also added some styling components with shadcn ui

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="bg-red-500 h-full">{children}</div>;
+  return (
+    <div className="bg-red-500 h-full flex justify-center items-center">
+      {children}
+    </div>
+  );
 };
 
 export default layout;
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,15 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
       <p className="text-3xl font-bold text-indigo-500">Hello Discord Clone</p>
       <Button>Click Me</Button>
+      <div>
+        <UserButton afterSignOutUrl="/" />
+        <ModeToggle />
+      </div>
     </div>
   );
 }
